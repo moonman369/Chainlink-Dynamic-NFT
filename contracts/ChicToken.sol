332c0939e37ab0d240816143c877fa40998ea4d1
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-05-07 23:43:22 +0530

    Add ChicToken ERC20 contract

---
+++ contracts/ChicToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ChicToken is ERC20, Ownable {
+
+    constructor (uint256 _totalSupply, address _tokenMintAddress) ERC20 ("ChicToken", "CHIC") {
+        _mint(_tokenMintAddress, _totalSupply);
+    }
+
+}
